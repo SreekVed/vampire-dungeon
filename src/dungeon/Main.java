1005dbed36301cc72952ba916ee971cfa358dd09
Sreekar Vedula <54064887+SreekarV@users.noreply.github.com>
2019-08-15 12:50:56 +0530

    Create Main.java

---
+++ src/dungeon/Main.java
@@ -0,0 +1,19 @@
+package dungeon;
+
+ 
+
+ 
+
+public class Main {
+
+    public static void main(String[] args) {
+
+       Dungeon game = new Dungeon(10,10,5,5,true);
+
+        game.run();
+
+    }
+
+}
+
+ 
